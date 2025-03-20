05197b30a4a8f24ea6b714c1df9492f53cb43d95
Hilixx <jhonalizan@gmail.com>
2025-03-20 14:27:58 -0300

    Removendo application.properties do controle do Git

---
+++ src/test/java/com/Libri/LibriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Libri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
