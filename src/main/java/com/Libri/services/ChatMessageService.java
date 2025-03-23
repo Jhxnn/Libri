12c6ce7d4a2ba080925d1ed6a42f8cb52ca207d1
Hilixx <jhonalizan@gmail.com>
2025-03-23 09:38:21 -0300

    teste

---
+++ src/main/java/com/Libri/services/ChatMessageService.java
@@ -0,0 +1,23 @@
+package com.Libri.services;
+
+
+import com.Libri.dtos.ChatDto;
+import com.Libri.models.ChatMessage;
+import com.Libri.repositories.ChatMessageRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatMessageService {
+
+    @Autowired
+    ChatMessageRepository chatMessageRepository;
+
+
+    public ChatMessage createMessage(ChatDto chatDto){
+        var chatMessage = new ChatMessage();
+        BeanUtils.copyProperties(chatDto, chatMessage);
+        return chatMessageRepository.save(chatMessage);
+    }
+}
