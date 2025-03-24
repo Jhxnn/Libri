f66770830029197ebea7c20a1a3efb57fd573e80
Hilixx <jhonalizan@gmail.com>
2025-03-23 23:14:10 -0300

    Auth finalizado a principio

--- src/main/java/com/Libri/services/UserService.java
+++ src/main/java/com/Libri/services/UserService.java
@@ -1,12 +1,17 @@
 package com.Libri.services;
 
 
+import com.Libri.dtos.AuthDto;
 import com.Libri.dtos.UserRequestDto;
 import com.Libri.dtos.UserResponseDto;
+import com.Libri.infra.security.TokenService;
 import com.Libri.models.User;
 import com.Libri.repositories.UserRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +22,19 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    EmailService emailService;
+
+
 
 
     public UserResponseDto updateUser(UUID id, UserRequestDto userRequestDto) {
@@ -30,6 +48,26 @@ public class UserService {
     public User findById1(UUID id){
         return userRepository.findById(id).orElseThrow(()-> new RuntimeException("Cannot be found"));
     }
+
+    public UserResponseDto createUser(UserRequestDto userRequestDto) {
+        if(userRepository.findByEmail(userRequestDto.email()) != null) return null;
+        String encryptedPass = new BCryptPasswordEncoder().encode(userRequestDto.password());
+
+        var user = new User();
+        BeanUtils.copyProperties(userRequestDto, user);
+        user.setPassword(encryptedPass);
+        emailService.enviarEmailTexto(user.getEmail(), "Conta criada","Bem vindo ao Libri " + user.getName() + ", faça bom uso :)");
+        userRepository.save(user);
+        return new UserResponseDto(user.getUserid(), user.getName(), user.getEmail());
+    }
+    public String returnToken(AuthDto authDto) {
+        var usernamePassord = new UsernamePasswordAuthenticationToken(authDto.email(), authDto.password());
+        var auth = this.authenticationManager.authenticate(usernamePassord);
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+        return token;
+    }
+
+
 
     public void deleteUser(UUID id) {
         var user = userRepository.findById(id).orElseThrow(()-> new RuntimeException("Cannot be found"));
