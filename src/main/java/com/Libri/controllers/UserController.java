65052ce63e5c127bc11c53e6cac2b443d764792f
Hilixx <jhonalizan@gmail.com>
2025-03-25 09:23:16 -0300

    usercontroller

--- src/main/java/com/Libri/controllers/UserController.java
+++ src/main/java/com/Libri/controllers/UserController.java
@@ -1,7 +1,62 @@
 package com.Libri.controllers;
 
+import com.Libri.dtos.AuthDto;
+import com.Libri.dtos.UserRequestDto;
+import com.Libri.dtos.UserResponseDto;
+import com.Libri.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+
+    @Operation(description="Lista todos os usuarios")
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    @Operation(description="Lista usuario pelo id")
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> findById(@PathVariable(name = "id") UUID id){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+    }
+
+    @Operation(description="Registra um usuario")
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto userRequestDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userRequestDto));
+    }
+
+    @Operation(description="Login de usuario")
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthDto authDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.returnToken(authDto));
+    }
+
+    @Operation(description="Atualiza um usuario")
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable(name = "id")UUID id,
+                                                      @RequestBody UserRequestDto userRequesDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.updateUser(id, userRequesDto));
+    }
+
+
+    @Operation(description="Deleta um usuario")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserResponseDto> deleteUser(@PathVariable(name = "id")UUID id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
